f3d095636ac3401d361ab869b46813b521afc143
unknown <andres.davila-g@labinfo.is.escuelaing.edu.co>
2022-01-20 16:20:27 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#suma de complejos
+def sumacplx(a,b):
+    # Use a breakpoint in the code line below to debug your script.
+    return (a[0]+b[0],a[1]+b[1])
+
+#resta de complejos
+def restacplx(a,b):
+    # Use a breakpoint in the code line below to debug your script.
+    return (a[0]-b[0],a[1]-b[1])
+
+# multiplicacion de complejos
+def multicplx(a,b):
+    real=(a[0]+b[0])-(a[1]+b[1])
+    complejo=(a[0]+b[1])+(a[1]+b[0])
+    return (real,complejo)
+# Division  de complejos
+def divcplx(a, b):
+    real=((a[0]*b[0])+(a[1]*b[1]))/((a[1]**2)+(b[1]**2))
+    img=((b[0]*a[1])-(a[0]*b[1]))/((a[1]**2)+(b[1]**2))
+    return (real,img)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(sumacplx((3.5,7),(4.2,8)))
+    print(multicplx((2,-2),(3,-2.3)))
+    print(restacplx((3.5, 7), (4.2, 8)))
+    print(divcplx((-2, 1), (1, 2)))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
